a00e91cd940a2bd038904c00ecea69ce87e047e7
Anifowose Temitayo <149629942+TheLius1@users.noreply.github.com>
2024-09-07 10:38:42 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
 const email = document.getElementById("email");
 const checkbox = document.getElementById("checkbox");
 const submitBtn = document.querySelector(".submit");
@@ -8,79 +10,90 @@ const confirmationMSG = document.querySelector(".confirmation__message");
 const query__errorMSG = document.querySelector(".query__error");
 const message__error = document.querySelector(".message__error");
 const errorMSG = document.querySelectorAll(".text__error");
-const nameInput = document.querySelectorAll(".nameInput");
 const inputType = document.querySelectorAll("input");
 const radioButtons = document.querySelectorAll("input[type= radio]");
-
-let isValid = true;
 
 //FUNCTION FOR ERROR HANDLING ON THE INPUT TYPES
 const errorHandler = function (input, index) {
   input.style.border = "1px solid var(--red)";
   errorMSG[index].classList.remove("hidden");
-  isValid = false;
 };
 const clearErrorHandler = function (input, index) {
   input.style.border = "";
   errorMSG[index].classList.add("hidden");
-  isValid = true;
 };
 
+let checkValidation = [];
+const addToValidation = (data) => {
+  if (!checkValidation.includes(data)) {
+    checkValidation.push(data);
+  }
+};
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  // FIRSTNAME && LASTNAME VALIDATION
-  nameInput.forEach((name, i) => {
-    if (name.value === "") {
-      errorHandler(name, i);
-    } else {
-      clearErrorHandler(name, i);
-    }
-  });
+  // FIRSTNAME SELECTOR VALUE AND INDEX
+  const firstnameValue = firstname.value.trim();
+  const firstnameIndex = Array.from(inputType).indexOf(firstname);
 
-  // EMAIL VALIDATION
+  if (firstnameValue === "") {
+    errorHandler(firstname, firstnameIndex);
+  } else {
+    clearErrorHandler(firstname, firstnameIndex);
+    addToValidation(firstname);
+  }
+
+  // LASTNAME SELECTOR VALUE AND INDEX
+  const lastnameValue = lastname.value.trim();
+  const lastnameIndex = Array.from(inputType).indexOf(lastname);
+
+  if (lastnameValue === "") {
+    errorHandler(lastname, lastnameIndex);
+  } else {
+    clearErrorHandler(lastname, lastnameIndex);
+    addToValidation(lastname);
+  }
+  // EMAIL SELECTOR VALUE, INDEX AND REGEX CODE
   const emailValue = email.value.trim();
   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   const emailIndex = Array.from(inputType).indexOf(email);
+
   if (!emailPattern.test(emailValue)) {
     errorHandler(email, emailIndex);
   } else {
     clearErrorHandler(email, emailIndex);
+    addToValidation(email);
   }
 
   // QUERY TYPE VALIDATION
   const checkedOne = Array.from(radioButtons).some((x) => x.checked);
   if (checkedOne === false) {
     query__errorMSG.classList.remove("hidden");
-    isValid = false;
   } else {
     query__errorMSG.classList.add("hidden");
-    isValid = true;
+    addToValidation(checkedOne);
   }
 
   // TEXTAREA VALIDATION
   if (textArea.value === "") {
     textArea.style.border = "1px solid var(--red)";
     message__error.classList.remove("hidden");
-    isValid = false;
   } else {
     textArea.style.border = "";
     message__error.classList.add("hidden");
-    isValid = true;
+    addToValidation(textArea);
   }
 
   // CHECKBOX VALIDATION
   const checkboxIndex = Array.from(inputType).indexOf(checkbox);
   if (!checkbox.checked) {
     errorMSG[checkboxIndex].classList.remove("hidden");
-    isValid = false;
   } else {
     errorMSG[checkboxIndex].classList.add("hidden");
-    isValid = true;
+    addToValidation(checkbox);
   }
-
   // SUCCESS VALIDATION
-  if (isValid) {
+  if (checkValidation.length >= 6) {
     confirmationMSG.classList.remove("hidden");
     setTimeout(() => {
       confirmationMSG.classList.add("hidden");
@@ -97,3 +110,4 @@ radioButtons.forEach((radio) => {
     e.target.closest(".query__box").classList.add("addChecked");
   });
 });
+
